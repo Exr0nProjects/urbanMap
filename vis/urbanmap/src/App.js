96a847b72f6ebc924ddc60be944568414f15f8e4
Exr0n <mail@exr0n.com>
2022-06-06 20:00:10 -0700

    init tailwind react app for use with deck.gl

---
+++ vis/urbanmap/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.png';
+
+import DeckGL from '@deck.gl/react';
+import {LineLayer} from '@deck.gl/layers';
+
+function App() {
+  return (
+      <div className="bg-gray-700 p-4 text-white w-screen h-screen overflow-none">
+      hewwwwwwwwwwww
+      </div>
+  );
+}
+
+export default App;
