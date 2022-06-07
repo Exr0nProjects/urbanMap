ca63b3fdddd300a83ede8df0efb3f7a839cdbc82
Exr0n <mail@exr0n.com>
2022-06-07 14:47:12 -0700

    custom viewstate example not working (state not updating?)

--- vis/urbanmap/src/App.js
+++ vis/urbanmap/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.png';
 
 import { COORDINATE_SYSTEM } from '@deck.gl/core';
@@ -20,6 +20,8 @@ const INITIAL_VIEW_STATE = {
 const COLUMNS = 'http://localhost:3000/data/cities.json';
 
 function App() {
+    const [viewState, setViewState] = useState(INITIAL_VIEW_STATE);
+
     const layers = [
         new ColumnLayer({
             id: 'population-columns',
@@ -30,31 +32,29 @@ function App() {
             opacity: 0.5,
             extruded: true,
             pickable: true,
-            elevationScale: 1e6,
+            //elevationScale: 1e6,
+            elevationScale: 1,
+            //getElevationScale: d => { console.log(viewState.zoom); return viewState.zoom },
             getPosition: d => [d.lon, d.lat],
             getFillColor: d => [48, 128, d.value * 255, 255],
-            getLineColor: (d, di) => { console.log(d, di); return [0, 0, 0]; },
-            getElevation: d => d.value
+            getLineColor: (d, di) => [0, 0, 0],
+            //getElevation: d => d.value / viewState.zoom
+            getElevation: d => { if (Math.random() < 1e-4) console.log(viewState.zoom); return viewState.zoom }
         }),
     ];
 
-    let zoom_reference = INITIAL_VIEW_STATE.zoom;
+    //let zoom_reference = INITIAL_VIEW_STATE.zoom;
 
     return (
         <div className="bg-gray-700 text-white w-screen h-screen overflow-hidden scroll-none">
         <div className="z-40 position-fixed h-20 w-40 border-2 border-blue-700 mb-0 ml-0">hewwo</div>
         <DeckGL
-            initialViewState={INITIAL_VIEW_STATE}
             controller={true}
+            viewState={viewState}
+            onViewStateChange={ e => { console.log('viewstatechanged', e.viewState.zoom); setViewState(e.viewState) } }
             layers={layers} >
             <Map
                 className="w-full h-full"
-                initialViewState={INITIAL_VIEW_STATE}
-                onViewStateChange={ ({viewState}) => {
-                    console.log(viewState);
-                    zoom_reference = viewState.zoom;
-                    return viewState;
-                } }
                 mapLib={maplibregl}
                 mapStyle="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
             />
