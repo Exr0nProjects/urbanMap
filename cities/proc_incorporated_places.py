1f44957220793edc44c1022f3f5566e12458b7c6
Exr0n <mail@exr0n.com>
2022-06-07 00:42:02 -0700

    move data relabelling to react code, unify labels in python; code cleaning

--- cities/proc_incorporated_places.py
+++ cities/proc_incorporated_places.py
@@ -4,8 +4,6 @@ import json
 
 def get_df_incorporated_places():
     FILENAME = "../data/incorporated_places.tsv"
-    # df = pd.read_csv(FILENAME, sep='	', dtype={ 'Place': str, 'Census': int, 'Estimates Base': int, **{f"20{y}": int for y in range(10, 20)} })
-    # df = pd.read_csv(FILENAME, sep='	', thousands=',')
     df = pd.read_csv(FILENAME, sep='	',  thousands=',')
     df = df.sort_values(by='2019', ascending=False)
     return df
@@ -14,30 +12,24 @@ def get_df_simplemaps_uscities_basic():
     FILENAME = "../data/simplemaps_uscities_basicv1.75/uscities.csv"
     df = pd.read_csv(FILENAME)
     df = df[['city', 'state_id', 'lat', 'lng', 'population', 'density']]
-    df = df.sort_values(by='population', ascending=False)
+    df = df.rename(columns={'state_id': 'state', 'lng': 'lon', 'population': 'pop'})
+    df = df.sort_values(by='pop', ascending=False)
     return df
 
 
 def fake_jsonl(data):
     return '[\n' + ',\n'.join('    ' + json.dumps(row) for row in data) + '\n]'
 
+def fake_jsonl_df(df):
+    return '[\n' + ',\n'.join('    ' + json.dumps({ k: v for k, v in row.items() }) for id, row in df.iterrows() ) + '\n]'
+
 if __name__ == '__main__':
-
-    # df = get_df_incorporated_places()
-    # print(df.head(20))
-
     df = get_df_simplemaps_uscities_basic()
 
-    print(df.head(20))
+    max_pop = max(df['pop'])
+    df['value'] = np.log(df['pop']) / np.log(max_pop * 2)
 
-    print([x for x in df.head(20).iterrows()])
+    # df.head(100).to_csv('top_100_cities.csv')
 
-    max_pop = max(df['population'])
-    df['population'] = np.log(df['population']) / np.log(max_pop * 2)
-    # max_pop = 1
-
-    simplemaps_basic = [{ 'city': f"{city:15s}", 'centroid': [lon, lat], 'pop': pop, 'density': dens } for id, (city, state, lat, lon, pop, dens) in df.iterrows()]
-    # simplemaps_basic = [{ 'city': city, 'posiiton': [lon, lat, 12], 'normal': [-1, 0, 0], 'color': [0x32, 0x6c, 0xcc] } for id, (city, state, lat, lon, pop, dens) in df.head(20).iterrows()]
-    # print(fake_jsonl(simplemaps_basic))
-    with open('../vis/prepped_data/simplemaps_basic_columns.json', 'w') as wf:
-        wf.write(fake_jsonl(simplemaps_basic))
+    with open('../vis/prepped_data/cities.json', 'w') as wf:
+        wf.write(fake_jsonl_df(df))
--- vis/urbanmap/src/App.js
+++ vis/urbanmap/src/App.js
@@ -6,9 +6,6 @@ import DeckGL from '@deck.gl/react';
 import { ColumnLayer, PointCloudLayer, GeoJsonLayer, ArcLayer } from '@deck.gl/layers';
 import { Map } from 'react-map-gl';
 import maplibregl from 'maplibre-gl';
-//import cities_population from './data/simplemaps_basic_columns.json';
-//import cities_locations_pointcloud from './data/simplemaps_basic_top20_pointcloud.json';
-//import 'maplibre-gl/dist/maplibre-gl.css';
 
 
 // Viewport settings
@@ -20,51 +17,9 @@ const INITIAL_VIEW_STATE = {
     bearing: 0
 };
 
-// Data to be used by the LineLayer
-//const data = [
-//    {sourcePosition: [-122.41669, 37.7853], targetPosition: [-122.41669, 37.781]}
-//];
-//
-
-
-// from https://github.com/visgl/deck.gl/blob/master/examples/get-started/react/arcgis/app.js
-const AIR_PORTS = 'https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_10m_airports.geojson';
-
-const COLUMNS = 'http://localhost:3000/data/simplemaps_basic_columns.json';
+const COLUMNS = 'http://localhost:3000/data/cities.json';
 
 function App() {
-
-    //const layers = [
-    //    new GeoJsonLayer({
-    //        id: 'airports',
-    //        data: AIR_PORTS,
-    //         Styles
-    //        filled: true,
-    //        pointRadiusMinPixels: 2,
-    //        pointRadiusScale: 2000,
-    //        getPointRadius: f => 11 - f.properties.scalerank,
-    //        getFillColor: [200, 0, 80, 180],
-    //         Interactive props
-    //        pickable: true,
-    //        autoHighlight: true,
-    //        onClick: info =>
-    //        info.object &&
-    //         eslint-disable-next-line
-    //        alert(`${info.object.properties.name} (${info.object.properties.abbrev})`)
-    //    }),
-    //    new ArcLayer({
-    //        id: 'arcs',
-    //        data: AIR_PORTS,
-    //        dataTransform: d => d.features.filter(f => f.properties.scalerank < 4),
-    //         Styles
-    //        getSourcePosition: f => [-0.4531566, 51.4709959],  London
-    //        getTargetPosition: f => f.geometry.coordinates,
-    //        getSourceColor: [0, 128, 200],
-    //        getTargetColor: [200, 0, 80],
-    //        getWidth: 1
-    //    })
-    //]
-
     const layers = [
         new ColumnLayer({
             id: 'column-layer',
@@ -76,24 +31,11 @@ function App() {
             extruded: true,
             pickable: true,
             elevationScale: 1e6,
-            getPosition: d => d.centroid,
+            getPosition: d => [d.lon, d.lat],
             getFillColor: d => [48, 128, d.value * 255, 255],
-            //getFillColor: d => [48, 128, 255, 255],
             getLineColor: [0, 0, 0],
             getElevation: d => d.value
-            //getElevation: d => 1000000
         }),
-    //    new PointCloudLayer({
-    //        id: 'point-cloud-layer',
-    //        cities_locations_pointcloud,
-    //        pickable: false,
-    //        coordinateSystem: COORDINATE_SYSTEM.LNGLAT,
-    //        coordinateOrigin: [-122.4, 37.74],
-    //        radiusPixels: 4,
-    //        getPosition: d => d.position,
-    //        getNormal: d => d.normal,
-    //        getColor: d => d.color
-    //    })
     ];
 
     return (
