e97b0e9b669a75f0fac34522b796baeb60ccd599
Exr0n <mail@exr0n.com>
2022-06-07 23:46:32 -0700

    Updates

---
+++ precache-manifest.c27e31b89db599116f5dec7aec2f7732.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3814242d0e876113591cb371ac7ed8f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46fe62c083cd63483bbb",
+    "url": "/static/css/main.3dd88101.chunk.css"
+  },
+  {
+    "revision": "424fffd0a4943548b8fb",
+    "url": "/static/js/2.c0ace6a9.chunk.js"
+  },
+  {
+    "revision": "90dbf267a3c36a05d4959064c92b77aa",
+    "url": "/static/js/2.c0ace6a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "881feb57f30184db3b4d",
+    "url": "/static/js/3.350f77d1.chunk.js"
+  },
+  {
+    "revision": "46fe62c083cd63483bbb",
+    "url": "/static/js/main.d1b92607.chunk.js"
+  },
+  {
+    "revision": "c915dbf68621f6e595fb",
+    "url": "/static/js/runtime-main.c08d078d.js"
+  },
+  {
+    "revision": "1ffea6bb8dccd42aa2b86c9157d5820a",
+    "url": "/static/media/logo.1ffea6bb.png"
+  }
+]);
