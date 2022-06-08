b04b14075afcf82361d364164c87d0c01e497744
Exr0n <mail@exr0n.com>
2022-06-07 17:01:52 -0700

    hexagon scaling not working

--- vis/urbanmap/src/App.js
+++ vis/urbanmap/src/App.js
@@ -4,6 +4,7 @@ import logo from './logo.png';
 import { COORDINATE_SYSTEM } from '@deck.gl/core';
 import DeckGL from '@deck.gl/react';
 import { ColumnLayer, PointCloudLayer, GeoJsonLayer, ArcLayer } from '@deck.gl/layers';
+import { HexagonLayer } from '@deck.gl/aggregation-layers';
 import { Map } from 'react-map-gl';
 import maplibregl from 'maplibre-gl';
 
@@ -25,7 +26,36 @@ function App() {
     const geo_elevation_scale_from_zoom = (zoom) => Math.min(Math.max(Math.pow(2.8, (18 - zoom)), 1e4), 1e6);
 
     const layers = [
+        new HexagonLayer({
+            id: 'population-hexagons',
+            data: POPULATION_COLUMNS_DATA,
+            pickable: true,
+            extruded: true,
+            radius: 1000,
+            elevationScale: 1,
+            getPosition: d => [d.lon, d.lat],
+
+            //getElevationWeight: d => d.value * geo_elevation_scale_from_zoom(viewState.zoom),
+            //elevationAggregation: 'SUM',
+            getElevationValue: d => {
+                //console.log(d);
+                //if (Math.random() < 1e-4) console.log('elevation = ',d.map(x => x.value).reduce((a, b) => a+b, 0) * geo_elevation_scale_from_zoom(viewState.zoom));
+                const sum_of_values = d.map(x => x.value).reduce((a, b) => a+b, 0);
+                if (Math.random() < 1e-4) console.log(sum_of_values, geo_elevation_scale_from_zoom(viewState.zoom));
+                return sum_of_values * geo_elevation_scale_from_zoom(viewState.zoom);
+            return d.value * geo_elevation_scale_from_zoom(viewState.zoom);
+            },
+
+            updateTriggers: {
+                getElevationValue: viewState.zoom,
+            }
+
+            //getColorValue: d => d.value,
+            //getFillColor: d => [48, 128, d.value * 255, 255],
+            //lineColor: [0, 0, 0],
+        }),
         new ColumnLayer({
+            visible: false,
             id: 'population-columns',
             data: POPULATION_COLUMNS_DATA,
             diskResolution: 40,
